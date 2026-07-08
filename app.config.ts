af9b852627bf0f9b806e5120307aed58a7f3c936
Franco Lo Cane <francolocane@MacBookAir.lan>
2026-07-08 16:57:13 +0200

    Fix ITMS-90683: add all iOS purpose strings

--- app.config.ts
+++ app.config.ts
@@ -7,6 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     ...(config.ios ?? {}),
     infoPlist: {
+      ...(config.ios?.infoPlist ?? {}),
       ITSAppUsesNonExemptEncryption: false,
     },
   },
